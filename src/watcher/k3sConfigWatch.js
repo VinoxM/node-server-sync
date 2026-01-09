d2288588b283b178bd7b57237e6fe35204326763
VinoxM <maou864@gmail.com>
2026-01-09 19:30:07 +0800

    update

--- src/watcher/k3sConfigWatch.js
+++ src/watcher/k3sConfigWatch.js
@@ -1,6 +1,7 @@
 import { Worker } from 'worker_threads';
 import { reloadApplicationContext } from '../support/index.js';
 import yaml from 'yaml';
+import { pushNotification } from '../sockets/notification.js';
 
 export function initK3SConfigurationWatcher() {
     const enabled = __env?.get('k3s.watch.enabled', false)
@@ -12,6 +13,11 @@ export function initK3SConfigurationWatcher() {
             const label = 'application-k3s-secret.yaml'
             const data = yaml.parse(message.data[label])
             reloadApplicationContext(['k3s-secret'], [{ data, label }])
+            const message = {
+                event: 'log',
+                message: `[K3S Configuration Watcher] Configuration changed: ${label}.`
+            }
+            pushNotification(JSON.stringify(message))
         }
     });
     logger(`[K3S Configuration Watcher] Initialized watcher.`);
