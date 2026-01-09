311cef024c21a7adf3ca538567a8e905661f64e7
VinoxM <maou864@gmail.com>
2026-01-09 17:28:10 +0800

    update

--- src/watcher/k3sConfigWatch.js
+++ src/watcher/k3sConfigWatch.js
@@ -10,7 +10,7 @@ export function initK3SConfigurationWatcher() {
         if (message.event === 'CONFIG_UPDATED') {
             logger(`[K3S Configuration Watcher] Configuration changed.`);
             console.log(message.data)
-            reloadApplicationContext(['k3s-secret'], yaml.parse(message.data))
+            reloadApplicationContext(['k3s-secret'], yaml.parse(message.data['application-k3s-secret.yaml']))
         }
     });
     logger(`[K3S Configuration Watcher] Initialized watcher.`);
