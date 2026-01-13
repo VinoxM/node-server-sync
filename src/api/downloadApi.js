cdb0c8afa68106ba97ff20d62df9bc9a84e3d64f
VinoxM <maou864@gmail.com>
2026-01-14 00:21:35 +0800

    updated

--- src/api/downloadApi.js
+++ src/api/downloadApi.js
@@ -2,7 +2,6 @@ import { checkBodyKeysNotBlank } from "../common/apiPreCheck.js";
 import apiMethodConst from "../constraints/apiMethodConst.js";
 import { addTorrent as aria2AddTorrent } from "../handler/download/aria2Handler.js";
 import { addTorrent as qbitAddTorrent } from "../handler/download/qbitorrentHandler.js";
-import { addTorrent as transmissionAddTorrent } from "../handler/download/transmissionHandler.js";
 
 const { POST } = apiMethodConst;
 
@@ -24,14 +23,6 @@ export default {
         preCheck: (req) => checkBodyKeysNotBlank(req, ['torrent']),
         callback: (req) => {
             return qbitAddTorrent(req.body.torrent)
-        }
-    },
-    "/transmission/addTorrent": {
-        method: POST,
-        needSecret,
-        preCheck: (req) => checkBodyKeysNotBlank(req, ['torrent']),
-        callback: (req) => {
-            return transmissionAddTorrent(req.body.torrent)
         }
     }
 }
