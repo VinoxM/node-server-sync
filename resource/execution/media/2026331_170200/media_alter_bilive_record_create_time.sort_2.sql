c66dc2069f000a4343b67627f4dd7979f85bb758
VinoxM <maou864@gmail.com>
2026-03-31 18:56:01 +0800

    fixed

--- resource/execution/media/2026331_170200/media_alter_bilive_record_create_time.sort_2.sql
+++ resource/execution/media/2026331_170200/media_alter_bilive_record_create_time.sort_2.sql
@@ -1,3 +1,3 @@
-ALTER TABLE bilive_record ADD COLUMN `create_time` datetime NOT NULL DEFAULT NULL;
+ALTER TABLE bilive_record ADD COLUMN `create_time` datetime DEFAULT NULL;
 
 UPDATE bilive_record SET create_time=event_timestamp;
