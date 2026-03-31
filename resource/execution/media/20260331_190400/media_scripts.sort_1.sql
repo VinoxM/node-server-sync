c4c052dbccc4b94762cf1e4369d091ac4a9af70d
VinoxM <maou864@gmail.com>
2026-03-31 19:05:38 +0800

    fixed

---
+++ resource/execution/media/20260331_190400/media_scripts.sort_1.sql
@@ -0,0 +1,2 @@
+UPDATE categories SET `type` = 1 WHERE name IN ('Hanime1', 'Adult');
+UPDATE bilive_record SET create_time=event_timestamp;
--- resource/execution/media/2026331_170200/media_alter_bilive_record_create_time.sort_2.sql
+++ resource/execution/media/2026331_170200/media_alter_bilive_record_create_time.sort_2.sql
@@ -1,3 +1,1 @@
 ALTER TABLE bilive_record ADD COLUMN `create_time` datetime DEFAULT NULL;
-
-UPDATE bilive_record SET create_time=event_timestamp;
