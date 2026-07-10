0fb5216365ce4eadc963842c4cfb1b153aacfd13
VinoxM <maou864@gmail.com>
2026-07-10 10:55:06 +0800

    update

--- install.js
+++ install.js
@@ -1,5 +1,18 @@
-import { pipeline } from '@xenova/transformers';
+import { pipeline, env } from '@xenova/transformers';
+import path from 'path'
 
-const extractor = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2', {
-    cache_dir: '/models'
-});
+env.allowRemoteModels = true;
+env.remoteHost = 'https://hf-mirror.com';
+
+async function download() {
+    console.log("正在从国内镜像站下载模型...");
+
+    // 执行下载
+    await pipeline('feature-extraction', 'Xenova/bge-small-zh-v1.5', {
+        cache_dir: path.resolve('/models')
+    });
+
+    console.log("模型成功下载到本地 /models 目录！");
+}
+
+download();
